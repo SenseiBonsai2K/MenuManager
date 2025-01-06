e0b412cc0acf556a5e3afa8531cbc490273e9e2b
Cristian Marinozzi <cristian.marinozzi1@gmail.com>
2025-01-06 18:31:13 +0100

    Aggiungere i file di progetto.

---
+++ MenuManager.Models/Entities/DishType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuManager.Models.Entities
+{
+    public class DishType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+
+
+        public DishType()
+        {
+
+        }
+
+        public DishType(int id, string type)
+        {
+            this.Id = id;
+            this.Type = type;
+        }
+    }
+}
